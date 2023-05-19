1a83635da27a6b149acf3e0d2aa52e3c1ce18c0a
Jordan Johnson <jordaninjohnson@gmail.com>
2023-05-18 22:56:01 -0600

    optionally display form

--- src/components/NewWound/NewWound.js
+++ src/components/NewWound/NewWound.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import WoundForm from './WoundForm';
 import './NewWound.css';
 
 const NewWound = (props) => {
+
+    const [ isEditing, setIsEditing ] = useState(false);
+
     const saveWoundDataHandler = (enteredWoundData) => {
         const woundData = {
             ...enteredWoundData,
             id: Math.random().toString()
         };
         props.onAddWound(woundData);
+        setIsEditing(false);
         // console.log(woundData);
     }
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    };
+
     return (
         <div className="new-wound">
-        <WoundForm onSaveWoundData={saveWoundDataHandler}/>
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add New Wound</button>
+            )}
+            {isEditing && (
+                <WoundForm
+                    onSaveWoundData={saveWoundDataHandler}
+                    onCancel={stopEditingHandler}
+                />
+            )}
         </div>
     );
 };
--- src/components/NewWound/WoundForm.js
+++ src/components/NewWound/WoundForm.js
@@ -86,7 +86,8 @@ const WoundForm = (props) => {
                 </div>
             </div>
             <div className="new-expense__actions">
-            <button type="submit" >Add Wound</button>
+            <button type="button" onClick={props.onCancel}>Cancel</button>
+            <button type="submit">Add Wound</button>
             </div>
         </form>
     );
